631ec8eb049871352116bd124a7011bc4b1ab975
Brady <brady.savarie@hotmail.com>
2023-02-19 14:59:39 -0500

    Progress on pre-game screen styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ['./*.html'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                Josefin: ['Josefin Sans', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 };
